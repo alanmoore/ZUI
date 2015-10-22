27ac4ddee8545f5db83b074b2c3fa64ad7822e4b
Alan Moore <contact@alanmoore.info>
2015-10-22 15:15:39 -0400

    Content is zooming correctly
    Content zooms on mask click
    'Zoom out' button appears
    Clicking 'Zoom out' makes content zoom out

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-
+    $('.zoomable--zoomed-out .content').addClass('content--zoomed-out');
 
 });
 
 $('.zoom-out-btn').click(function() {
+    /*
     var fakeZoomer = $('#zoom-animation-holder');
 
     if (fakeZoomer.hasClass('has-content')) {
@@ -21,20 +22,17 @@ $('.zoom-out-btn').click(function() {
 
 
     var targetDiv = $('.zoomable--zoomed-in:not(:has(".zoomable--zoomed-in"))');
-    var targetContent = $('.content--zoomed-in:not(:has(".content--zoomed-in"))'); 
-    var mask = targetDiv.siblings('.mask');
-      
-    targetDiv.removeClass('zoomable--zoomed-in').addClass('zoomable--zoomed-out');
-    targetContent.removeClass('content--zoomed-in');
-    targetContent.removeAttr('style');
-    mask.removeClass('hidden');
-    $(".zoomable, .pages").animate({ scrollTop: 0}, { duration: 'slow', easing: 'swing'});
+    */
+    //var mask = targetDiv.siblings('.mask');
+    var targetContent = $('.content--zoomed-in');
 
+    targetContent.removeClass('content--zoomed-in').addClass('content--zoomed-out');  
+    //mask.removeClass('hidden');
+    
     var checkFor = $('.zoomable--zoomed-in'); 
     if (checkFor.length < 1) {
         $(this).css('opacity', '0');
     }
-
 });
 
 $('.mask').click(function() {
@@ -44,6 +42,7 @@ $('.mask').click(function() {
     var targetContent = targetDiv.children('.content');
     var zoomOut = $('.zoom-out-btn');
     var mask = $(this).siblings('.mask');
+    /*
     var targetOffset = targetDiv.offset();
     var thisX = targetOffset.left;
     var thisY = targetOffset.top;
@@ -51,16 +50,13 @@ $('.mask').click(function() {
     var thisHeight = targetDiv.height();
     var cssString = "translate(-"+thisX+"px,-"+thisY+"px)";
     console.log(cssString);
-
-  /*
-  
- targetDiv.removeClass('zoomable--zoomed-out').addClass('zoomable--zoomed-in');
-  targetContent.addClass('content--zoomed-in');
-  targetContent.css('transform', cssString);
-  */
  
     var fakeZoomer = $('#zoom-animation-holder');
+    */
 
+    targetContent.removeClass('content--zoomed-out').addClass('content--zoomed-in');
+
+    /*
     fakeZoomer.css({
         'position': 'absolute',
         'left':     thisX,
@@ -75,15 +71,17 @@ $('.mask').click(function() {
         fakeZoomer.removeClass('has-content');
         console.log('cleared out fakeZoomer');
     } 
-  
+    
     targetDiv.clone().appendTo(fakeZoomer);
+    
+
     fakeZoomer.addClass('has-content');
     console.log('cloned content');
     fakeZoomer.removeClass('zoomable--zoomed-out').addClass('zoomable--zoomed-in');
-    var fakeTargetContent = fakeZoomer.children('.content');
+    var fakeTargetContent = fakeZoomer.children('.zoomable');
     fakeTargetContent.addClass('content--zoomed-in');
     fakeTargetContent.css('transform', cssString);
-   
+    */
     window.scrollTo(0, 0);
 
     zoomOut.css('opacity', '1');
